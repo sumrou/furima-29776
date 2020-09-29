d7cb2bd1282df000814a70db4fc8734fbb8ba347
sumrou <69175299+sumrou@users.noreply.github.com>
2020-09-29 16:01:41 +0900

    新規登録のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'には全角文字を使用してください' } do
+validates :first_name_kanji, :last_name_kanji
+end
+validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'はカタカナで入力して下さい'}
+validates :nickname, :birthday, presence:true
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
 end
