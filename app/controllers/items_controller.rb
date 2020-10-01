1ce2268ccd062a4330bbb84a9a4ab8b65c672db5
sumrou <69175299+sumrou@users.noreply.github.com>
2020-10-01 15:00:23 +0900

    ログインユーザーのみが出品ページに飛べる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,16 @@
 class ItemsController < ApplicationController
+before_action :move_to_sign_in, except: [:index, :show]
 
   def index
+  end
+
+  def new
+  end
+
+  private
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
