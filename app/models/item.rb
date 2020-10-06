5f1a0d9079c5dfd402e91910a8b48d57a522e17c
sumrou <69175299+sumrou@users.noreply.github.com>
2020-10-06 13:08:04 +0900

    出品機能モデルテストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, numericality: { only_integer: true, greater_than: 1, message: 'を選択してください' }
   validates :name, :introduction, :price, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, presence: true
-  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000, message: 'は300~9,999,999の間で入力してください' }
   validates :price, format: { with: /\A\d{3,7}\z/, message: 'は半角数字で入力してください'}
-  
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 describe User do
- before do
-   @user = FactoryBot.build(:user)
- end
+
 
  describe 'ユーザー新規登録' do
    context '新規登録がうまくいくとき' do
