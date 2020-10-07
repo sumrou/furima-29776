200b5cc3667c09d6804b54deb8eb7c75e309bc1b
sumrou <69175299+sumrou@users.noreply.github.com>
2020-10-07 14:44:46 +0900

    商品一覧/ログアウト状態でも一覧が見られる

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../items")
+require("../items_new")
+require("../items_index")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :user do
     transient do
-      person {Gimei.name}
+      person { Gimei.name }
     end
 
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {'abc123'}
-    password_confirmation {password}
-    first_name_kanji      {'名字'}
-    last_name_kanji       {'名前'}
-    first_name_kana       {'ミョウジ'}
-    last_name_kana        {'ナマエ'}
-    birthday              {'2020-09-01'}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'abc123' }
+    password_confirmation { password }
+    first_name_kanji      { '名字' }
+    last_name_kanji       { '名前' }
+    first_name_kana       { 'ミョウジ' }
+    last_name_kana        { 'ナマエ' }
+    birthday              { '2020-09-01' }
   end
 end
