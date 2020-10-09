a6ff5c12037b8311ccb467b2cd60dd54f63c63c3
sumrou <69175299+sumrou@users.noreply.github.com>
2020-10-09 14:46:18 +0900

    URL直接入力でedit画面に飛べてしまうのを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_sign_in
-    redirect_to new_user_session_path unless user_signed_in?
+    unless user_signed_in?
+    redirect_to new_user_session_path
+    end
   end
 
   def set_item
