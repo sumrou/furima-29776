9d82fae66cbf023dbe54b57b6147707a916b098c
sumrou <69175299+sumrou@users.noreply.github.com>
2020-10-09 13:06:16 +0900

    rubocopで修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,13 +44,10 @@ class ItemsController < ApplicationController
   end
 
   def move_to_sign_in
-    unless user_signed_in?
-    redirect_to new_user_session_path 
-    end
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   def set_item
     @item = Item.find(params[:id])
   end
-  
 end
